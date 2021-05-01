58e692bba98ee155a242d863a8994e2de7817beb
Ehsan Noori <ehsanynoori@gmail.com>
2021-05-01 15:36:22 +0800

    content added to flask files

--- flaskFiles/app.py
+++ flaskFiles/app.py
@@ -28,6 +28,37 @@ def login():
 
     return render_template('login.html', error=error)
 
+@app.route('/learningcontent', methods=['GET', 'POST'])
+def learningcontent():
+
+    error = None
+    return render_template('Page1.html')
+
+@app.route('/introduction', methods=['GET', 'POST'])
+def introduction():
+
+    error = None
+    return render_template('Introduction.html')
+
+@app.route('/basics', methods=['GET', 'POST'])
+def basics():
+
+    error = None
+    return render_template('Basics.html')
+
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+
+    error = None
+    return render_template('Quiz.html')
+
+@app.route('/self-paced-project', methods=['GET', 'POST'])
+def self():
+
+    error = None
+    return render_template('Self.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
