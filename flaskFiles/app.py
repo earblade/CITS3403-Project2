aac204438a13ecf0c0b44320fafc80a51113d1a2
earblade <tjlknuckles@hotmail.com>
2021-05-04 14:59:20 +0800

    initialised jinja to modularise html

--- flaskFiles/app.py
+++ flaskFiles/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('homepage.html')
+    return render_template('homepage.html', title="Learn React")
 
 
 @app.route('/about')
@@ -27,6 +27,72 @@ def login():
             return redirect(url_for('home'))
 
     return render_template('login.html', error=error)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = None
+
+    if request.method == 'POST':
+        if request.form['username'] != username or request.form['password'] != password:
+            error = "Incorrect input"
+        else:
+            return redirect(url_for('home'))
+    return render_template('signup.html', error=error)
+
+@app.route('/Page1.html', methods=['GET', 'POST'])
+def learningcontent():
+
+    error = None
+    return render_template('Page1.html')
+
+@app.route('/Introduction.html', methods=['GET', 'POST'])
+def introduction():
+
+    error = None
+    return render_template('Introduction.html')
+
+@app.route('/Basics.html', methods=['GET', 'POST'])
+def basics():
+
+    error = None
+    return render_template('Basics.html')
+
+@app.route('/Rendering.html', methods=['GET', 'POST'])
+def rendering():
+
+    error = None
+    return render_template('Rendering.html')
+
+@app.route('/Components.html', methods=['GET', 'POST'])
+def components():
+
+    error = None
+    return render_template('Components.html')
+
+@app.route('/Handling.html', methods=['GET', 'POST'])
+def handling():
+
+    error = None
+    return render_template('Handling.html')
+
+@app.route('/Conditional.html', methods=['GET', 'POST'])
+def conditional():
+
+    error = None
+    return render_template('Conditional.html')
+
+@app.route('/Quiz.html', methods=['GET', 'POST'])
+def quiz():
+
+    error = None
+    return render_template('Quiz.html')
+
+@app.route('/Self.html', methods=['GET', 'POST'])
+def self():
+
+    error = None
+    return render_template('Self.html')
 
 
 if __name__ == '__main__':
