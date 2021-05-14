2e2a57d32cb8fcfdd3c992da65719e0b40885fe3
Ethan <ethanpui@gmail.com>
2021-05-14 15:36:09 +0800

    Quiz color change

--- flaskFiles/app.py
+++ flaskFiles/app.py
@@ -22,6 +22,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+    # test = db.Column(db.Integer())
 
 
 @login_manager.user_loader
